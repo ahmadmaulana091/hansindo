c8b7dbe158587c4e9071ef2b928569fead49bab9
ahmadmaulana091 <ahmadmaulana091@gmail.com>
2026-07-31 17:10:37 +0700

    feat: Revisi satu

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -121,7 +121,7 @@ export const About = () => {
               {/* Main photo */}
               <div className="relative rounded-2xl overflow-hidden shadow-xl aspect-[4/3]">
                 <Image
-                  src="/images/truk_hansindo.png"
+                  src="/images/truk_putih_lift_load.png"
                   alt="Armada Truk Hansindo Logistik"
                   fill
                   className="object-cover"
