9f2d2e8d0385465add3195f1ee8a97b476fd3454
Ahmad Maulana <internys220@ysrcpafirm.co.id>
2026-09-17 11:33:13 +0700

    fix: add force-static

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://hansindo.co.id";
+  const baseUrl = "https://hansindologistik.com";
 
   return [
     {
